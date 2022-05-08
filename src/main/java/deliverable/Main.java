f3b1097e229755f66251e31569d0ee2d7b36fc62
Fanfarillo <matteo.fanfarillo99@gmail.com>
2022-05-08 22:41:18 +0200

    Fixed bug on exception declaration

--- src/main/java/deliverable/Main.java
+++ src/main/java/deliverable/Main.java
@@ -3,13 +3,15 @@ package deliverable;
 import java.io.IOException;
 import java.text.ParseException;
 
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.RevisionSyntaxException;
 import org.json.JSONException;
 
 import deliverable.control.ExecutionFlow;
 
 public class Main {
 
-	public static void main(String[] args) throws JSONException, IOException, ParseException {
+	public static void main(String[] args) throws JSONException, IOException, ParseException, RevisionSyntaxException, GitAPIException {
 		
 		ExecutionFlow.collectData("avro");
 		//Same with bookkeeper
